17f3fab1f75d98885fa05808c5f34c8ccfbeb806
TonyZhang19950601 <youzhang1995@gmail.com>
2017-08-28 15:28:12 -0400

    388 Longest Absolute File Path

---
+++ Favorite_Questions/src/LongestAbsoluteFilePath.java
@@ -0,0 +1,71 @@
+/**
+ * Created by youzhang on 2017/8/28.
+ */
+import java.util.Stack;
+/*
+* The string is in essence the preorder traversal of the directory tree. We need to find the longest way(count on characters,
+* not levels) from root to leaf(a file). Thus, for every directory, we push the length from root to it into the stack, for every file,
+* we examine the length from root to it and see whether it's larger than the current max.
+* to find a parent of the current dir/file, we only need to find the first dir in the stack whose depth is smaller than the current depth(
+* the depth can be counted based on the prefix \t)
+* */
+public class LongestAbsoluteFilePath {
+    public int lengthLongestPath(String input) {
+        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> depthStack = new Stack<Integer>();
+        int max = 0;
+        int current = 0;
+        boolean isFile = false;
+        int currentDepth = 0;
+        for(int i=0;i<input.length();i++){
+            char c = input.charAt(i);
+            if(c=='\n') {
+                //find the parent dir in the tree
+                while(!depthStack.isEmpty()&&depthStack.peek()>=currentDepth){
+                    stack.pop();
+                    depthStack.pop();
+                }
+                int lastDepth = 0;
+                if(!stack.isEmpty()){
+                    lastDepth = stack.peek();
+                }
+                if(isFile){
+                    max = Math.max(lastDepth+current, max);
+                }
+                else{
+                    stack.push(lastDepth+current+1);
+                    depthStack.push(currentDepth);
+                }
+                currentDepth = 0;
+                //count the depth in the tree
+                while(i+1<input.length()&&input.charAt(i+1)=='\t'){
+                    i++;
+                    currentDepth++;
+                }
+                current = 0;
+                isFile = false;
+            }
+            else{
+                if(c=='.'){
+                    isFile = true;
+                }
+                current++;
+            }
+        }
+        //deal with the last file in th string
+        if(isFile){
+            int lastDepth = 0;
+            while(!depthStack.isEmpty()&&depthStack.peek()>=currentDepth){
+                stack.pop();
+                depthStack.pop();
+            }
+            if(!stack.isEmpty()){
+                lastDepth = stack.peek();
+            }
+            if(lastDepth+current>max){
+                max = lastDepth+current;
+            }
+        }
+        return max;
+    }
+}
